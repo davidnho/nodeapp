343da0baa982902cb02eeddbb2c6549ea2329a43
Noel David <noelsdavid@gmail.com>
2016-12-16 15:12:56 +0800

    bootstrap

--- noel_review/app/routes/index.js
+++ noel_review/app/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
 
-  res.render('index', {
-    pageTitle : "Index page",
+  res.render('home', {
+    pageTitle : "Welcome to My Page",
     pageId : "index"
   });
 
